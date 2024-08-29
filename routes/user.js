e41bdb0b61dc6e53b209f167cf41371ffb5777a2
TrustyKrab <fathihpra6303@gmail.com>
2024-08-29 08:18:49 +0700

    add forgotPass

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,13 @@ import { User } from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import nodemailer from 'nodemailer'
+
+
+
 dotenv.config();
 const router = express.Router();
 
-router.get("/getUsers", async(req, res) => {
+router.get("/getUsers", async (req, res) => {
     try {
         const users = await User.find();
         res.json(users)
@@ -16,7 +19,7 @@ router.get("/getUsers", async(req, res) => {
     }
 })
 
-router.get("/:id/getUserByID", async(req, res) => {
+router.get("/:id/getUserByID", async (req, res) => {
     try {
         const users = await User.findById(req.params.id);
         res.json(users)
@@ -26,13 +29,13 @@ router.get("/:id/getUserByID", async(req, res) => {
     }
 })
 
-router.patch('/:id/updateUser' , async (req, res) => {
+router.patch('/:id/updateUser', async (req, res) => {
     try {
-        const updateUser = await User.updateOne({_id:req.params.id}, {$set : req.body})
-        res.status(201).json({message : "berhasil mengupdate", updateUser})
+        const updateUser = await User.updateOne({ _id: req.params.id }, { $set: req.body })
+        res.status(201).json({ message: "berhasil mengupdate", updateUser })
     } catch (error) {
         console.log(error)
-        return res.status(400).json({message: "terjadi kesalahan"})
+        return res.status(400).json({ message: "terjadi kesalahan" })
     }
 })
 
@@ -51,13 +54,13 @@ router.get("/getUserByUsername", async (req, res) => {
     }
 });
 
-router.delete('/:id/deleteUser' , async (req, res) => {
+router.delete('/:id/deleteUser', async (req, res) => {
     try {
-        const deleteUser = await User.deleteOne({_id:req.params.id})
-        res.status(201).json({message : "berhasil menghapus", deleteUser})
+        const deleteUser = await User.deleteOne({ _id: req.params.id })
+        res.status(201).json({ message: "berhasil menghapus", deleteUser })
     } catch (error) {
         console.log(error)
-        return res.status(400).json({message: "terjadi kesalahan"})
+        return res.status(400).json({ message: "terjadi kesalahan" })
     }
 })
 
@@ -84,7 +87,7 @@ router.post('/register', async (req, res) => {
         });
 
         await newUser.save();
-        return res.json({status: true, message: "User registered successfully" });
+        return res.json({ status: true, message: "User registered successfully" });
     } catch (error) {
         console.log(error);
         return res.status(400).json({ message: "Error registering user" });
@@ -96,18 +99,18 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(404).json({status: false, message: "Akun tidak ditemukan" });
+            return res.status(404).json({ status: false, message: "Akun tidak ditemukan" });
         }
 
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
-            return res.status(400).json({status: false, message: "Password yang Anda masukkan salah" });
+            return res.status(400).json({ status: false, message: "Password yang Anda masukkan salah" });
         }
-        
+
 
         const token = jwt.sign({ username: user.username }, process.env.KEY, { expiresIn: '1h' });
         res.cookie('token', token, { httpOnly: true, maxAge: 360000 });
-        return res.json({  message: "Login berhasil", token });
+        return res.json({ message: "Login berhasil", token });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Terjadi kesalahan pada server" });
@@ -115,31 +118,92 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/submitresult', async (req, res) => {
-    const { percoobaan , username, score, quizname} = req.body;
-    const user = await User.findOne({username});
+    const { percoobaan, username, score, quizname } = req.body;
+    const user = await User.findOne({ username });
 
-    if(!user) {
-        return res.status(404).json({message : "usertidak ditemukan"})
+    if (!user) {
+        return res.status(404).json({ message: "usertidak ditemukan" })
     }
 
     try {
-        if(!user.quiz){
-            user.quiz = [{percoobaan : 1}]
+        if (!user.quiz) {
+            user.quiz = [{ percoobaan: 1 }]
         }
 
-        user.quiz.push({percoobaan : user.quiz.length + 1, score, quizname})
+        user.quiz.push({ percoobaan: user.quiz.length + 1, score, quizname })
 
         await user.save()
-        return res.status(200).json({message : "niali Quiz terekap"})
+        return res.status(200).json({ message: "niali Quiz terekap" })
     } catch (error) {
         console.log(error)
-        return res.status(400).json({message : "terjadi kesalahan saat melakukan submit"})
+        return res.status(400).json({ message: "terjadi kesalahan saat melakukan submit" })
     }
 })
 
+router.post('/forgotpassword', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ status: false, message: "Akun tidak ditemukan" });
+        }
+
+
+        const token = jwt.sign({ id: user._id }, process.env.KEY, { expiresIn: '10m' });
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        var mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Reset Password',
+            text: `https://lutfi-script-client.vercel.app/resetpassword/${token}`
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return res.json({ message: "Error sending email" });
+            } else {
+                return res.json({ status: true, message: "Email sent" });
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post('/resetpassword', async (req, res) => {
+    const { password, token } = req.body;
+
+    try {
+        const decoded = jwt.verify(token, process.env.KEY);
+        const userId = decoded.id;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const hashpassword = await bcrypt.hash(password, 10);
+        user.password = hashpassword;
+        await user.save();
+
+        return res.json({ status: true, message: "Password berhasil diubah" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Terjadi kesalahan pada server" });
+    }
+});
+
 router.get('/logout', (req, res) => {
     res.clearCookie('token')
-    return res.json({status : true})
+    return res.json({ status: true })
 })
 
 export { router as UserRouter };
