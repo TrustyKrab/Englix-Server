0cbbd658e57e2731d4c0ed9c1d3cacd4be337e38
TrustyKrab <fathihpra6303@gmail.com>
2024-08-31 04:27:02 +0700

    TRY EGEN 1

--- index.js
+++ index.js
@@ -41,8 +41,6 @@ app.use('/user', UserRouter)
 app.use('/user/:id', UserRouter)
 
 
-
-
 mongoose.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB");
